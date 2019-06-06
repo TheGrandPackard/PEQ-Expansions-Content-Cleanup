43d71229f9a797f9cbad73c8edfc96f488346539
Joshua Packard <josh@hotelwifi.com>
2019-06-06 16:49:25 -0700

    Added spawns

--- zones/feerrott/README.md
+++ zones/feerrott/README.md
@@ -1,1 +1,15 @@
-Zone Cleanup
+# Feerrott Zone Cleanup
+
+## Spawns
+
+* Annaelia Wylassi (47154) disabled until RoK
+* Aqaar Aluram (47132) disabled until LoY
+* Emissary Shinsere (47068) disabled until LoY
+* Grinak (47153) disabled until GoD and after RoS - Emissary of Shadowrest
+* Radiant (47200) disabled until LDoN
+* Scout Ahlikal (47197) disabled until SoL
+* Spanner Scrapsnatcher (47130) disabled until LoY
+
+## Merchant Lists
+
+## Loot Tables
---
+++ zones/feerrott/feerrott.sql
@@ -0,0 +1,14 @@
+-- Annaelia Wylassi (47154) disabled until RoK|
+UPDATE spawn2 SET min_expansion=2 WHERE id=5236;
+-- Aqaar Aluram (47132) disabled until LoY|
+UPDATE spawn2 SET min_expansion=6 WHERE id=5197;
+-- Emissary Shinsere (47068) disabled until LoY|
+UPDATE spawn2 SET min_expansion=6 WHERE id=4958;
+-- Grinak (47153) disabled until GoD and after RoS - Emissary of Shadowrest|
+UPDATE spawn2 SET min_expansion=8, max_expansion=25 WHERE id=5235;
+-- Radiant (47200) disabled until LDoN|
+UPDATE spawn2 SET min_expansion=7 WHERE id=5261;
+-- Scout Ahlikal (47197) disabled until SoL|
+UPDATE spawn2 SET min_expansion=4 WHERE id=5236;
+-- Spanner Scrapsnatcher (47130) disabled until LoY|
+UPDATE spawn2 SET min_expansion=6 WHERE id=5195;
