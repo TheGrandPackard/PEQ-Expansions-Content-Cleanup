b1ce57306248fb43e6002c6bfdd94cb84502cad9
Joshua Packard <thegrandpackard@gmail.com>
2019-06-06 15:22:14 -0700

    Add NPC spawns

---
+++ zones/felwithea/felwithea.sql
@@ -0,0 +1,27 @@
+-- NPC Spawns
+-- Serenica Greenfinger (61029) disabled until OoW - Quest mob	
+UPDATE spawn2 SET min_expansion=9 WHERE id=8748;
+-- Alladria Skyetcher (61030) unknown era spawn	
+UPDATE spawn2 SET min_expansion=70 WHERE id=8749;
+-- Feraniel Leafsway (61031) unknown era spawn	
+UPDATE spawn2 SET min_expansion=70 WHERE id=8750;
+-- Laernian Caelael (67001) unknown era spawn - Town Crier	
+UPDATE spawn2 SET min_expansion=70 WHERE id=8784;
+-- Galrun Starforge (61065) disabled until Omens of War - Paladin 2.0 Epic mob	
+UPDATE spawn2 SET min_expansion=9 WHERE id=250548;
+-- Ann Oma (61023) disabled until GoD - Tribute Master	
+UPDATE spawn2 SET min_expansion=8 WHERE id=8742;
+-- a rodent (61000) disabled until LoY - Extermination quest mob	
+UPDATE spawn2 SET min_expansion=6 WHERE id IN (8721, 8722, 8723, 8724, 8725, 8726, 8727, 8728, 8729, 8730);
+-- Bedida Claygrinder (61028) unknown era spawn - Tradeskill merchant	
+UPDATE spawn2 SET min_expansion=70 WHERE id=8747;
+-- Exterminator Valern (61014) disabled until LoY - Extermination quest mob	
+UPDATE spawn2 SET min_expansion=6 WHERE id=8733;
+-- Larroniae Huial (61070) disabled until LDoN - Adventurer's Stone quest	
+UPDATE spawn2 SET min_expansion=7 WHERE id=8789;
+-- Soulbinder Elendalira (61067) unknown era spawn - Soulbinder	
+UPDATE spawn2 SET min_expansion=70 WHERE id=8786;
+
+-- Merchant Lists
+
+-- Spawns
