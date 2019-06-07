abd23c92a272b704691b64d0b49e966578ad8c73
Joshua Packard <josh@hotelwifi.com>
2019-06-07 14:22:57 -0700

    Added spawns

--- zones/crushbone/README.md
+++ zones/crushbone/README.md
@@ -1,1 +1,11 @@
-Zone Cleanup
+# Crushbone Zone Cleanup
+
+## Spawns
+
+* an orc thaumaturgist (58043) unknown era spawn
+* Orc Scoutsman (58042) unknown era spawn
+* Orc Warden (58047) unknown era spawn
+
+## Merchant Lists
+
+## Loot Tables
---
+++ zones/crushbone/crushbone.sql
@@ -0,0 +1,11 @@
+-- Spawns
+-- an_orc_thaumaturgist
+UPDATE spawn2 SET min_expansion=70 WHERE id=112;
+-- Orc_Scoutsman
+UPDATE spawn2 SET min_expansion=70 WHERE id=109;
+-- Orc_Warden
+UPDATE spawn2 SET min_expansion=70 WHERE id=113;
+
+-- Merchant Lists
+
+-- Loot Tables
