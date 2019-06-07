cc573b2b896f4c9428f46f65b374c6aeabf631d7
Joshua Packard <josh@hotelwifi.com>
2019-06-07 11:24:20 -0700

    Add spawns

--- zones/arena/README.md
+++ zones/arena/README.md
@@ -1,1 +1,14 @@
-Zone Cleanup
+# The Arena Zone Cleanup
+
+## Spawns
+
+* an undead training dummy (77003) unknown era spawn
+* a training dummy (77002) unknown era spawn
+* DPS Machine (77004) unknown era spawn
+* DPS Machine (77005) unknown era spawn
+* Soulbinder Gendal (77000) disabled until SoL - Soulbinder
+
+## Merchant Lists
+
+## Loot Tables
+
---
+++ zones/arena/arena.sql
@@ -0,0 +1,15 @@
+-- NPC Spawns
+-- an undead training dummy unknown era spawn
+UPDATE spawn2 SET min_expansion=70 WHERE id=68527;
+-- a training dummy unknown era spawn
+UPDATE spawn2 SET min_expansion=70 WHERE id=68500;
+-- DPS Machine unknown era spawn
+UPDATE spawn2 SET min_expansion=70 WHERE id=137543;
+-- DPS Machine unknown era spawn
+UPDATE spawn2 SET min_expansion=70 WHERE id=156657;
+-- Soulbinder Gendal disabled until SoL - Soulbinder
+UPDATE spawn2 SET min_expansion=4 WHERE id=57775;
+
+-- Merchant Lists
+
+-- Loot Tables
