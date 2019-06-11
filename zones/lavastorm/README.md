fded0b28e43dd80d360950a96657f0da119847a6
Joshua Packard <josh@hotelwifi.com>
2019-06-11 16:16:18 -0700

    Add zone points

--- zones/lavastorm/README.md
+++ zones/lavastorm/README.md
@@ -4,3 +4,10 @@
 
 * Jenah Wheelspinner (27019)
   * Earring of Fire Reflection (10303) probable accidental entry
+
+## Zone Points
+
+* The Broodlands (1881) unknown era spawn point
+* Lavaspinner's Lair (1876) unknown era spawn point
+* Nektulos Forest [revamp] (1588) unknown era spawn point
+
--- zones/lavastorm/lavastorm.sql
+++ zones/lavastorm/lavastorm.sql
@@ -1,3 +1,11 @@
 -- Merchant List
 -- Jenah Wheelspinner
 UPDATE merchantlist SET min_expansion=70 WHERE merchantid=27019 and item=10303;
+
+-- Zone Points
+-- The Broodlands
+UPDATE zone_points SET min_expansion=70 WHERE id=1881;
+-- Lavaspinner's Lair
+UPDATE zone_points SET min_expansion=70 WHERE id=1876;
+-- Nektulos Forest [revamp]
+UPDATE zone_points SET min_expansion=70 WHERE id=1588;
