1943224a1059df243486cd10ca0fbac6c1eb6f63
Joshua Packard <josh@hotelwifi.com>
2019-06-12 10:26:05 -0700

    Cleaned up disabling spawn entries instead of spawn2 so that classic spawns still work

--- zones/kedge/kedge.sql
+++ zones/kedge/kedge.sql
@@ -1,13 +1,17 @@
 -- kedge
 -- Spawns
 -- Auraline
-UPDATE spawn2 SET min_expansion=24 WHERE id=4704;
+UPDATE spawnentry SET chance=24 WHERE spawngroupID=2137 AND npcID=64037;
+UPDATE spawnentry SET chance=24 WHERE spawngroupID=2140 AND npcID=64037;
+UPDATE spawnentry SET chance=24 WHERE spawngroupID=2166 AND npcID=64037;
 -- Nyrein_the_Beguiled
 UPDATE spawn2 SET min_expansion=5 WHERE id=4725;
 -- Priestess_Auraia
 UPDATE spawn2 SET min_expansion=5 WHERE id=4729;
 -- tainted_seahorse
-UPDATE spawn2 SET min_expansion=2 WHERE id=4785;
+UPDATE spawnentry SET chance=2 WHERE spawngroupID=6488 AND npcID=64048;
+UPDATE spawnentry SET chance=2 WHERE spawngroupID=6490 AND npcID=64048;
+UPDATE spawnentry SET chance=2 WHERE spawngroupID=65493 AND npcID=64048;
 -- Undertow
 UPDATE spawn2 SET min_expansion=2 WHERE id=4794;
 
